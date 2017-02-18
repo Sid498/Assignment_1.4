5c0d110e64ca7bb697e9aef0dac5706c9e8dbd67
Sid498 <sidpawar619@gmail.com>
2017-02-18 22:25:05 +0530

    Assignment_1.4

---
+++ src/Ascii.java
@@ -0,0 +1,11 @@
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		//char will convert int to char
+		for (int i = 65; i <= 90; i++) {
+			System.out.println(i+"-"+(char)i);
+		}
+	}
+
+}
